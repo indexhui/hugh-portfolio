0ddf75764eeb0b5fd8bf412778d3a52a1dc6b817
hugh <indexhui@gmail.com>
2022-10-20 20:10:34 +0800

    home layout

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -23,7 +23,7 @@ const ProjectCard = props => {
 
   return (
     <Flex w="100%" direction={{ base: 'column', lg: 'row' }} align="center">
-      <Flex w="50%" as={RouterLink} to={props.link}>
+      <Flex w={{ base: '100%', lg: '40%' }} as={RouterLink} to={props.link}>
         <AspectRatio w="100%" ratio={5 / 3}>
           <Flex w="100%" bgColor="#EDEEF0" borderRadius="md" overflow="hidden">
             <AspectRatio w="100%" ratio={5 / 3}>
@@ -39,15 +39,22 @@ const ProjectCard = props => {
         </AspectRatio>
       </Flex>
       <VStack
+        py={{ base: '16px', lg: '0' }}
+        w={{ base: '100%', lg: '60%' }}
         px={{ lg: '24px' }}
         align="flex-start"
-        spacing={{ base: '4px', lg: '16px' }}
+        spacing={{ base: '2px', lg: '12px' }}
       >
-        <Text fontSize="20px" fontWeight="600">
+        <Text textStyle="text01" fontWeight="600">
           {props.title}
         </Text>
-        <Text color="grey.500">{props.tag}</Text>
-        <Text>{props.tag}</Text>
+        <Text textStyle="text02" color="grey.700">
+          {props.subTitle}
+        </Text>
+        <Text textStyle="text04" color="blue.700">
+          {props.tag}
+        </Text>
+        <Text textStyle="text04">{props.content}</Text>
         <Text>{isNeedPassword ? '需要密碼' : ''}</Text>
       </VStack>
     </Flex>
--- src/components/home/DesignProjectList.js
+++ src/components/home/DesignProjectList.js
@@ -1,4 +1,5 @@
-import { Flex, Text, VStack, Heading } from '@chakra-ui/react';
+import { Flex, VStack, Heading } from '@chakra-ui/react';
+import { Container, SkeletonImage } from 'components/layouts';
 
 import ProjectCard from 'components/ProjectCard';
 
@@ -9,37 +10,48 @@ import project03 from 'assets/images/project03.jpg';
 const projectList = [
   {
     title: 'Crosspoint  AI 體態分析 ',
-    tag: 'App Design, UI/UX Design, Prototyping, User Research',
+    subTitle: '運健檢測系統',
+    tag: 'App Design, UI/UX Design, User Research',
     img: project02,
     link: '/crosspoint',
     isNeedPassword: false,
+    content:
+      'Crosspoint 透過攝影機捕捉人體動態變化搭配 AI 影像分析， 提供健身與復健場域，利用 AI 姿勢評估檢測系統，去了解運動時身體平衡差異與肌肉緊繃概況，讓教練以資訊視覺化圖像來提供會員建議。',
   },
   {
     title: 'Real Engine 真實引擎',
-    tag: 'App Design, UI/UX Design, Prototyping, User Research',
+    subTitle: 'IP 創作者桌遊募資',
+    tag: ' UI/UX Design, Web app',
     img: project01,
     link: '/realengine',
     content:
-      '設計產品的 MVP 版本且順利上線。我也定義了產品團隊第一套設計系統與設計元件庫。',
+      '從頭開始規劃專屬於 IP 創作者的桌遊募資平台，因而定義了三項圍繞 IP 創作者優勢的設計策略',
   },
   {
     title: 'Portaly 傳送門',
-    tag: '桌遊資訊',
+    subTitle: 'Link-in-Bio 輕型個人網站',
+    tag: '創作者工具, Web app',
     img: project03,
     link: '/portaly',
-    isNeedPassword: true,
+    content:
+      '提供創作者們，一個較低成本、好上手的個人頁面建立工具，放大圖文的吸引力、藉此增加點擊與互動的機會，讓受眾得以透過「傳送門 Portaly」觸及他們喜愛的創作者，擴散創作的影響力。',
+    // isNeedPassword: true,
   },
 ];
 
 const DesignProjectList = () => {
   return (
     <Flex w="100%" justify="center" py={{ base: '48px', lg: '32px' }}>
-      <VStack w="75%" maxW="1220px" spacing={{ base: '24px', lg: '48px' }}>
-        <Heading as="h2">設計案例</Heading>
-        {projectList.map(project => (
-          <ProjectCard key={project.title} {...project} />
-        ))}
-      </VStack>
+      <Container>
+        <Heading as="h2" py="40px">
+          設計案例
+        </Heading>
+        <VStack w="100%" maxW="1220px" spacing={{ base: '16px', lg: '48px' }}>
+          {projectList.map(project => (
+            <ProjectCard key={project.title} {...project} />
+          ))}
+        </VStack>
+      </Container>
     </Flex>
   );
 };
