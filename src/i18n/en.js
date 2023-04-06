ca71f43122d7acd1ce6a079bf28639699e8cc719
hugh <indexhui@gmail.com>
2023-04-07 07:29:06 +0800

    intro update

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -3,7 +3,7 @@ const aboutPage = {
   aboutSlogan01: 'Providing people with the help they need through observation',
   aboutSlogan02: 'is where my passion as a designer lies.',
   aboutIntro01:
-    'Hello everyone, I am Hugh, a Product manager with development and design experience.',
+    'Hello everyone, I am Hugh, a Product manager / designer with development and design experience.',
   aboutIntro02: `Five years of product design and over a year of front-end development experience. Through assuming different roles, I have explored and summarized different possibilities for team collaboration and design trends."`,
   aboutIntro03:
     'I am committed to creating smooth interactions and enjoyable experiences between people and products. At the same time, I strongly believe that holistic strategic thinking is helpful in making designs practical and turning concepts into reality. I enjoy the process and sense of accomplishment that comes with being a designer and having the opportunity to contribute to human progress.',
